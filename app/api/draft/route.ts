b087d04117f643c2dbcd81d4c8812987290c6b99
Reda <mahmoudreda457@gmail.com>
2026-07-05 10:03:58 +0300

    create a draft api route for preview CMS content

---
+++ app/api/draft/route.ts
@@ -0,0 +1,22 @@
+import { draftMode } from "next/headers";
+import { redirect } from "next/navigation";
+import { NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const secret = searchParams.get("secret");
+
+  if (!secret) {
+    return new Response("No", { status: 400 });
+  }
+
+  if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
+    return new Response("No", { status: 401 });
+  }
+
+  const draft = await draftMode();
+  draft.enable();
+
+  // Redirect to the draft page (homepage since we are previewing the hero section)
+  redirect("/");
+}
